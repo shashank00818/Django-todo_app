a1ed3048517f85b6339e5005bf50febfa505b94f
shashank00818 <shashank00818@gmail.com>
2023-06-18 21:11:50 +0530

    adding items in todo completed

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import todo
 
 # Create your views here.
+def home(request):
+    data = todo.objects.all()
+    return render(request, 'home.html', {'data': data})
+
+def add(request):
+    tododata = request.POST['todo']
+    todo_item = todo(content=tododata)
+    todo_item.save()
+    return redirect(home)
--- todo_app/urls.py
+++ todo_app/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todo.views import home,add
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
+    path('add/', add, name="add"),
 ]
